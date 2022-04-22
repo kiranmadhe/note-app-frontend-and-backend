534a680e5c520c7fb7f5eaa52c6700aca7a8069d
kiranmadhe <kiranmadhe2000@gmail.com>
2022-04-22 20:51:22 +0530

    fetchuser/get user data added

---
+++ backend/middleware/fetchuser.js
@@ -0,0 +1,26 @@
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET="noteapp"
+
+const fetchuser=('./fetchuser', (req,res, next)=>{
+    const token = req.header('auth-token');
+
+    if(!token){
+        return res.status(401).json({ errors: "Invalid Credentials" });
+    }
+
+    try{
+    const data = jwt.verify(token,JWT_SECRET)
+    req.user = data.user
+    next();
+
+    }catch (error) {
+        console.log(error.mesage);
+        res.status(500).json({ errors: "some error occured" });
+    }
+
+})
+
+
+
+module.exports = fetchuser;
